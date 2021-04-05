28672eec920fc5b7b8a4f393817206e0d4117e19
danxinyuan1999 <danxinyuan1999@foxmail.com>
2021-04-05 21:27:41 +0800

    feat: 添加主启动类

---
+++ xy_oa_system_api/src/main/java/com/dan/XyOaSpringApplication.java
@@ -0,0 +1,15 @@
+package com.dan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后台服务主启动类
+ * @author dan
+ */
+@SpringBootApplication
+public class XyOaSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XyOaSpringApplication.class, args);
+    }
+}
